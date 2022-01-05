0044c5b1328bfc6857ad1c8364f190773ffb94c8
Kanimozhi04 <96735864+Kanimozhi04@users.noreply.github.com>
2022-01-05 18:29:26 +0530

    Update script.js

--- Task4/script.js
+++ Task4/script.js
@@ -30,8 +30,8 @@
 			x++;
 		}
               var selectedRow = null;
-function onFormSubmit(){
-   
+function onFormSubmit(e){
+    event.preventDefault();
     var formData = readFormData();
     if(selectedRow === null){
         insertNewRecord(formData);
@@ -73,7 +73,7 @@ function insertNewRecord(data){
    var cell6 = newRow.insertCell(5);
         cell6.innerHTML = data.dept;
    var cell7 = newRow.insertCell(6);
-        cell7.innerHTML = data.dob;
+        cell7.innerHTML = data.address;
     var cell8 = newRow.insertCell(7);
         cell8.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button onClick='onDelete(this)'>Delete</button>`
 }
@@ -81,33 +81,41 @@ function insertNewRecord(data){
 //Edit the data
 function onEdit(td){
     selectedRow = td.parentElement.parentElement;
-    document.getElementById('productCode').value = selectedRow.cells[0].innerHTML;
-    document.getElementById('product').value = selectedRow.cells[1].innerHTML;
-    document.getElementById('qty').value = selectedRow.cells[2].innerHTML;
-    document.getElementById('perPrice').value = selectedRow.cells[3].innerHTML;
+    document.getElementById('name').value = selectedRow.cells[0].innerHTML;
+    document.getElementById('mobileno').value = selectedRow.cells[1].innerHTML;
+    document.getElementById('email').value = selectedRow.cells[2].innerHTML;
+    document.getElementById('gender').value = selectedRow.cells[3].innerHTML;
+    document.getElementById('dob').value = selectedRow.cells[3].innerHTML;
+    document.getElementById('dept').value = selectedRow.cells[3].innerHTML;
+    document.getElementById('address').value = selectedRow.cells[3].innerHTML;
 }
 
 function updateRecord(formData){
-    selectedRow.cells[0].innerHTML = formData.productCode;
-    selectedRow.cells[1].innerHTML = formData.product;
-    selectedRow.cells[2].innerHTML = formData.qty;
-    selectedRow.cells[3].innerHTML = formData.perPrice;
+    selectedRow.cells[0].innerHTML = formData.name;
+    selectedRow.cells[1].innerHTML = formData.mobileno;
+    selectedRow.cells[2].innerHTML = formData.email;
+    selectedRow.cells[3].innerHTML = formData.dob;
+    selectedRow.cells[4].innerHTML = formData.dept;
+    selectedRow.cells[5].innerHTML = formData.address;
 }
 
 //Delete the data
 function onDelete(td){
     if(confirm('Do you want to delete this record?')){
         row = td.parentElement.parentElement;
-        document.getElementById('storeList').deleteRow(row.rowIndex);
+        document.getElementById('register').deleteRow(row.rowIndex);
     }
     resetForm();
 }
 
 //Reset the data
 function resetForm(){
-    document.getElementById('productCode').value = '';
-    document.getElementById('product').value = '';
-    document.getElementById('qty').value = '';
-    document.getElementById('perPrice').value = '';
+    document.getElementById('name').value = '';
+    document.getElementById('mobileno').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('gender').value = '';
+    document.getElementById('dob').value = '';
+    document.getElementById('dept').value = '';
+    document.getElementById('address').value = '';
 }
 			
