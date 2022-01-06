4118e6a9310d71314c3bc55dbaa1a99d8ef376d8
Kanimozhi04 <96735864+Kanimozhi04@users.noreply.github.com>
2022-01-06 18:06:23 +0530

    Update script.js

--- Task4/script.js
+++ Task4/script.js
@@ -1,37 +1,7 @@
-               var list1 = [];
-		var list2 = [];
-		var list3 = [];
-		var list4 = [];
 
-		var n = 1;
-		var x = 0;
-
-		function AddRow(){
-
-			var AddRown = document.getElementById('show');
-			var NewRow = AddRown.insertRow(n);
-
-			list1[x] = document.getElementById("name").value;
-			list2[x] = document.getElementById("mobileno").value;
-			list3[x] = document.getElementById("email").value;
-			list4[x] = document.getElementById("male").value;
-
-			var cel1 = NewRow.insertCell(0);
-			var cel2 = NewRow.insertCell(1);
-			var cel3 = NewRow.insertCell(2);
-			var cel4 = NewRow.insertCell(3);
-
-			cel1.innerHTML = list1[x];
-			cel2.innerHTML = list2[x];
-			cel3.innerHTML = list3[x];
-			cel4.innerHTML = list4[x];
-
-			n++;
-			x++;
-		}
-              var selectedRow = null;
-function onFormSubmit(e){
-    event.preventDefault();
+var selectedRow = null;
+function onFormSubmit(){
+ 
     var formData = readFormData();
     if(selectedRow === null){
         insertNewRecord(formData);
@@ -48,17 +18,16 @@ function readFormData(){
     formData["name"] = document.getElementById("name").value;
     formData["mobileno"] = document.getElementById("mobileno").value;
     formData["email"] = document.getElementById("email").value;
-    formData["gender"] = document.getElementById("gender").value;
+    /*formData["gender"] = document.getElementById("gender").value;*/
     formData["dob"] = document.getElementById("dob").value;
     formData["dept"] = document.getElementById("dept").value;
     formData["address"] = document.getElementById("address").value;
-
     return formData;
 }
 
 //Insert the data
 function insertNewRecord(data){
-    var table = document.getElementById("register").getElementsByTagName('tbody')[0];
+    var table = document.getElementById('register').getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     var cell1 = newRow.insertCell(0);
         cell1.innerHTML = data.name;
@@ -66,16 +35,15 @@ function insertNewRecord(data){
         cell2.innerHTML = data.mobileno;
     var cell3 = newRow.insertCell(2);
         cell3.innerHTML = data.email;
+
     var cell4 = newRow.insertCell(3);
-        cell4.innerHTML = data.gender;
-   var cell5 = newRow.insertCell(4);
-        cell5.innerHTML = data.dob;  
-   var cell6 = newRow.insertCell(5);
-        cell6.innerHTML = data.dept;
-   var cell7 = newRow.insertCell(6);
-        cell7.innerHTML = data.address;
-    var cell8 = newRow.insertCell(7);
-        cell8.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button onClick='onDelete(this)'>Delete</button>`
+        cell4.innerHTML = data.dob;
+    var cell5= newRow.insertCell(4);
+        cell5.innerHTML = data.dept;
+    var cell6= newRow.insertCell(5);
+        cell6.innerHTML = data.address;
+    var cell7 = newRow.insertCell(6);
+        cell7.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button onClick='onDelete(this)'>Delete</button>`
 }
 
 //Edit the data
@@ -84,16 +52,15 @@ function onEdit(td){
     document.getElementById('name').value = selectedRow.cells[0].innerHTML;
     document.getElementById('mobileno').value = selectedRow.cells[1].innerHTML;
     document.getElementById('email').value = selectedRow.cells[2].innerHTML;
-    document.getElementById('gender').value = selectedRow.cells[3].innerHTML;
     document.getElementById('dob').value = selectedRow.cells[3].innerHTML;
-    document.getElementById('dept').value = selectedRow.cells[3].innerHTML;
-    document.getElementById('address').value = selectedRow.cells[3].innerHTML;
+    document.getElementById('dept').value = selectedRow.cells[4].innerHTML;
+    document.getElementById('address').value = selectedRow.cells[5].innerHTML;
 }
 
 function updateRecord(formData){
     selectedRow.cells[0].innerHTML = formData.name;
     selectedRow.cells[1].innerHTML = formData.mobileno;
-    selectedRow.cells[2].innerHTML = formData.email;
+    selectedRow.cells[2].innerHTML = formData.mailid;
     selectedRow.cells[3].innerHTML = formData.dob;
     selectedRow.cells[4].innerHTML = formData.dept;
     selectedRow.cells[5].innerHTML = formData.address;
@@ -113,9 +80,7 @@ function resetForm(){
     document.getElementById('name').value = '';
     document.getElementById('mobileno').value = '';
     document.getElementById('email').value = '';
-    document.getElementById('gender').value = '';
     document.getElementById('dob').value = '';
     document.getElementById('dept').value = '';
     document.getElementById('address').value = '';
 }
-			
