4a44c218fd6e1830a74559fc3424a18a80c677ea
Kanimozhi04 <96735864+Kanimozhi04@users.noreply.github.com>
2022-01-21 12:06:42 +0530

    Update script.js

--- Task6/script.js
+++ Task6/script.js
@@ -1,7 +1,15 @@
-var box=document.querySelectorAll(".box");
-for(i=0;i<box.length;i++)
-{
-    box[i].addEventListener("click",function(){
-        this.classList.toggle("active")
-    })
+var tabs = document.querySelectorAll(".collapseTab");
+for ( let i = 0; i < tabs.length; i++) {
+tabs[i].addEventListener("click", function() {
+    var content = tabs[i].nextElementSibling;
+    if (content.style.maxHeight)
+    {
+       content.style.maxHeight = null;
+    }
+    else 
+    {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+    tabs[i].classList.toggle("active");
+});
 }
