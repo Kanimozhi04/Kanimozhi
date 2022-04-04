3d6b0f3a64cf153b83331c4dbe11ff7b03600d2e
Kanimozhi04 <96735864+Kanimozhi04@users.noreply.github.com>
2022-04-04 12:55:05 +0530

    Update script.js

--- Task7/script.js
+++ Task7/script.js
@@ -10,7 +10,7 @@ for (i = 0; i < btns.length; i++) {
         console.log(filter);
         
         storeProducts.forEach((product)=> {
-            if (filter === ''){
+            if (filter == ''){
                 product.style.display = 'block'
             } else {
                 if (product.classList.contains(filter)){
